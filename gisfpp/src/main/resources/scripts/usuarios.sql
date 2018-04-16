e303c916b74171f8d58d2f4cd64b6cef02c553d2
Roberto Voogt <robertoc.voogt@gmail.com>
2018-04-15 23:21:53 -0300

    Se agrega que se creen usuarios para todas las personas

--- gisfpp/src/main/resources/scripts/usuarios.sql
+++ gisfpp/src/main/resources/scripts/usuarios.sql
@@ -40,3 +40,18 @@ INSERT INTO `usuario`(`activo`, `nickname`, `password`, `personaId`) select 1,no
 INSERT INTO `usuario`(`activo`, `nickname`, `password`, `personaId`) select 1,nombre,nombre, idPersona from persona where idPersona =	191;
 INSERT INTO `usuario`(`activo`, `nickname`, `password`, `personaId`) select 1,nombre,nombre, idPersona from persona where idPersona =	192;
 INSERT INTO `usuario`(`activo`, `nickname`, `password`, `personaId`) select 1,nombre,nombre, idPersona from persona where idPersona =	193;
+
+-- Crear usuarios para las personas que aun no tengan y que no esten duplicadas:
+insert into usuario (activo, nickname, password,personaId) 
+select 1,nombre, nombre,idPersona 
+	from persona 
+	-- Que no tenga usuario ya creado
+	where idPersona not in (select personaId from usuario) 
+		-- Que no este duplicado sino tira error de UK en la tabla de usuarios
+		and idPersona not in (SELECT per1.idPersona
+								FROM persona per1
+								   INNER JOIN (SELECT per2.idPersona, per2.nombre
+								               FROM   persona per2
+								               GROUP  BY per2.nombre
+								               HAVING COUNT(per2.nombre) > 1) dup
+								           ON per1.nombre = dup.nombre)
