43c7b261d1601f907d244833db073014801b442a
Roberto <Roberto@DESKTOP-KFBU7MS>
2018-05-10 18:56:31 -0300

    TDD en clase

--- gisfpp/src/main/java/unpsjb/fipm/gisfpp/dao/proyecto/DaoProyecto.java
+++ gisfpp/src/main/java/unpsjb/fipm/gisfpp/dao/proyecto/DaoProyecto.java
@@ -1,8 +1,13 @@
 package unpsjb.fipm.gisfpp.dao.proyecto;
 
+import java.util.List;
+
 import unpsjb.fipm.gisfpp.dao.DaoGenerico;
+import unpsjb.fipm.gisfpp.entidades.proyecto.OfertaActividad;
 import unpsjb.fipm.gisfpp.entidades.proyecto.Proyecto;
 
 public interface DaoProyecto extends DaoGenerico<Proyecto, Integer> {
 
+	List<OfertaActividad> getAllOfertas();
+		
 }
