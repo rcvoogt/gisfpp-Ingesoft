882a828877ebdb6cb41da3436225eb1a62e46740
Unknown <lucasboba@gmail.com>
2018-06-05 16:00:39 -0300

    Scripts
    Correccion de fechas en scripts de proyecto e isfpp

--- gisfpp/src/main/resources/scripts/sprint3/setIsfpp.sql
+++ gisfpp/src/main/resources/scripts/sprint3/setIsfpp.sql
@@ -1,11 +1,11 @@
 INSERT INTO `isfpp`(`id`, `detalle`, `estado`, `fecha_fin`, `fecha_inicio`, `objetivos`, `titulo`, `subProyectoId`)
 VALUES (1,'Isfpp creada de forma estatica, para probar datos','GENERADA','2018-12-12','2018-02-12','Isfpp creada de forma estatica, para probar datos','Sistema en Grid para cultivos',2),
-(2,'Isfpp creada de forma estatica, para probar datos','GENERADA','2018-12-12','2018-02-12','Isfpp creada de forma estatica, para probar datos','Sistema en Grid para cultivos',2),
+(2,'Isfpp creada de forma estatica, para probar datos','GENERADA','2018-12-12','2018-02-12','Isfpp creada de forma estatica, para probar datos','Sistema en Grid para cultivos 2',2),
 (3,'Isfpp creada de forma estatica, para probar datos','RECHAZADA','2018-12-12','2018-03-12','Isfpp creada de forma estatica, para probar datos','CORREL. GEOLÓGICAS Y PALEOMAG',3),
 (4,'Isfpp creada de forma estatica, para probar datos','CONCLUIDA','2018-12-12','2018-01-12','Isfpp creada de forma estatica, para probar datos','PRODUCCION DE COLZA-CANOLA',4),
 (5,'Isfpp creada de forma estatica, para probar datos','SUSPENDIDA','2018-12-12','2018-01-12','Isfpp creada de forma estatica, para probar datos','DESARROLLO MODULOS CLIMATICOS',5);
 
-INSERT INTO `staff_isfpp`(`idStaffIsfpp`, `rol`, `isfppId`, `personaId`) 
+INSERT INTO `staff_isfpp`(`idStaffIsfpp`, `rol`, `isfppId`, `personaId`)
 VALUES (1,'TUTOR_ACADEMICO',1,1),
 (2,'TUTOR_EXTERNO',1,2),
 (3,'TUTOR_ACADEMICO',2,3),
--- gisfpp/src/main/resources/scripts/sprint3/setProyecto.sql
+++ gisfpp/src/main/resources/scripts/sprint3/setProyecto.sql
@@ -3,11 +3,10 @@ INSERT INTO `proyecto`(`idProyecto`, `codigo`, `descripcion`, `detalle`, `estado
 VALUES (1,1425,'Proyecto concluido Nº1','Datos para pruebas de aceptación ','CONCLUIDO' ,'2017-03-01' ,'2017-09-01' ,'Resolucion Nº 0001', 'EXTENSION' ,'Sistema Monitoreo invernáculo'),
 (2,9115,'Proyecto concluido Nº2','Datos para pruebas de aceptación','CONCLUIDO' ,'2017-03-01' ,'2017-09-01' ,'Resolucion Nº 0002', 'EXTENSION' ,'Sistema en Grid para cultivos'),
 (3,8926,'Proyecto concluido Nº3','Datos para pruebas de aceptación','CONCLUIDO','2017-03-01' ,'2017-09-01' ,'Resolucion Nº 0003', 'EXTENSION' ,'CORREL. GEOLÓGICAS Y PALEOMAG'),
-(4,9537,'Proyecto activo Nº1','Datos para pruebas de aceptación','ACTIVO' ,'2018-03-01' ,'2018-09-01' ,'Resolucion Nº 0004', 'EXTENSION' ,'PRODUCCION DE COLZA-CANOLA'),
-(5,10418,'Proyecto activo Nº2','Datos para pruebas de aceptación','ACTIVO' ,'2018-03-01' ,'2018-09-01' ,'Resolucion Nº 0005', 'INVESTIGACION' ,'DESARROLLO MODULOS CLIMATICOS'),
-(6,9919,'Proyecto activo Nº3','Datos para pruebas de aceptación','ACTIVO' ,'2018-03-01' ,'2018-09-01' ,'Resolucion Nº 0006', 'INVESTIGACION' ,'Plataforma para ACC'),
-(7,8505,'Proyecto activo Nº4','Datos para pruebas de aceptación','ACTIVO' ,'2018-03-01' ,'2018-09-01' ,'Resolucion Nº 0007', 'INTERNO' ,'Ce-Bios'),
-(8,9845,'Proyecto activo Nº5','Datos para pruebas de aceptación','ACTIVO' ,'2018-03-01' ,'2018-09-01' ,'Resolucion Nº 0008', 'INTERNO' ,'Arandu Recursos Biológicos'),
-(9,10692,'Proyecto activo Nº6','Datos para pruebas de aceptación','ACTIVO' ,'2018-03-01' ,'2018-09-01' ,'Resolucion Nº 0009', 'EMPRESA' ,'Predictor de CiberAtacantes'),
-(10,11111,'Proyecto activo Nº7','Datos para pruebas de aceptación','ACTIVO' ,'2018-03-01' ,'2018-09-01' ,'Resolucion Nº 0010', 'EMPRESA' ,'Monitoreo de ganado ovino');
-
+(4,9537,'Proyecto activo Nº1','Datos para pruebas de aceptación','ACTIVO' ,'2017-11-01' ,'2019-09-01' ,'Resolucion Nº 0004', 'EXTENSION' ,'PRODUCCION DE COLZA-CANOLA'),
+(5,10418,'Proyecto activo Nº2','Datos para pruebas de aceptación','ACTIVO' ,'2017-11-01' ,'2019-09-01' ,'Resolucion Nº 0005', 'INVESTIGACION' ,'DESARROLLO MODULOS CLIMATICOS'),
+(6,9919,'Proyecto activo Nº3','Datos para pruebas de aceptación','ACTIVO' ,'2017-11-01' ,'2019-09-01' ,'Resolucion Nº 0006', 'INVESTIGACION' ,'Plataforma para ACC'),
+(7,8505,'Proyecto activo Nº4','Datos para pruebas de aceptación','ACTIVO' ,'2017-11-01' ,'2019-09-01' ,'Resolucion Nº 0007', 'INTERNO' ,'Ce-Bios'),
+(8,9845,'Proyecto activo Nº5','Datos para pruebas de aceptación','ACTIVO' ,'2017-11-01' ,'2019-09-01' ,'Resolucion Nº 0008', 'INTERNO' ,'Arandu Recursos Biológicos'),
+(9,10692,'Proyecto activo Nº6','Datos para pruebas de aceptación','ACTIVO' ,'2017-11-01' ,'2019-09-01' ,'Resolucion Nº 0009', 'EMPRESA' ,'Predictor de CiberAtacantes'),
+(10,11111,'Proyecto activo Nº7','Datos para pruebas de aceptación','ACTIVO' ,'2017-11-01' ,'2019-09-01' ,'Resolucion Nº 0010', 'EMPRESA' ,'Monitoreo de ganado ovino');
