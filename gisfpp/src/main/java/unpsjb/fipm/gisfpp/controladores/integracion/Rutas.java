4d22fb61ceb0a67820a17f0d93a2a913c4ecfd04
IsaiasArza <arza.isaias@gmail.com>
2018-07-01 17:08:31 -0300

    Rutas
    Agrego ruta de alumno_cursada

--- gisfpp/src/main/java/unpsjb/fipm/gisfpp/controladores/integracion/Rutas.java
+++ gisfpp/src/main/java/unpsjb/fipm/gisfpp/controladores/integracion/Rutas.java
@@ -1,6 +1,6 @@
 package unpsjb.fipm.gisfpp.controladores.integracion;
 
-import java.net.URI;
+
 
 public class Rutas {
 	
@@ -10,6 +10,6 @@ public class Rutas {
 	public static final String SERVICIO_CURSADA_TEST = "http://localhost:30005/listado/cursadas";
 	
 	public static final String SERVICIO_MATERIA = "http://fipmif015.ddns.net:31002/materias/getMaterias";
-
+	public static final String SERVICIO_CURSADA_ALUMNOS = "http://http://fipmif015.ddns.net:31002/services/alumnosComisiones/getAlumnosCursadas";
 
 }
